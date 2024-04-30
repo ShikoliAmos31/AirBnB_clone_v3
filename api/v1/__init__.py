b96fa7f8bf963d68906966036f64d16e0acdbbd8
shikoliamos31 <amosshikoli@gmail.com>
2024-04-30 06:25:11 -0700

    State

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,3 +2,13 @@
 """
 views
 """
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if __name__ == '__main__':
+    pass
+
+from api.v1.views.index import *
+from api.v1.views.states import *
